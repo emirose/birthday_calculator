6a6b211575705cdb78d5f5e17b89cd7dece378ed
Emily Rosengren <erosengr@thoughtworks.com>
2014-06-09 16:12:05 -0500

    step 1: setup tornado app and static html page with form

--- app.py
+++ app.py
@@ -4,19 +4,14 @@ import tornado
 import tornado.web
 import tornado.httpserver
 
-import models.birthday_calculator as birthday_calculator
-
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", CalculatorHandler) ,
-            (r"/calculate", CalculatorHandler)
                 ]
         settings = dict(
                 template_path=os.path.join(os.path.dirname(__file__),
                     'templates'),
-                static_path=os.path.join(os.path.dirname(__file__),
-                    'static'),
                 debug = True
                 )
         tornado.web.Application.__init__(self, handlers, **settings)
@@ -24,12 +19,7 @@ class Application(tornado.web.Application):
 class CalculatorHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.render('index.html', results=None)
-
-    def post(self):
-        birthdate = self.get_argument('birthdate')
-        results = birthday_calculator.calculate(birthdate)
-        self.render('index.html', results=results)
+        self.render('index.html')
 
 def main():
     Application().listen(8888)
